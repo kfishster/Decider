9b36bde86cd80478f31be1b59d0fc1ad4fb366ff
kfishster <kirillvar@gmail.com>
2013-04-01 04:04:36 -0500

    adding add event functionality

--- newEventForm.php
+++ newEventForm.php
@@ -1,12 +1,12 @@
 
 <h3>Create a new event</h3>
 <form class="form-horizontal" id="submitEvent">
-  <div class="control-group">
+  
     <label class="control-label">Title</label>
-    <div class="controls">
+    
       <input type="text" id="titleinp" placeholder="Title">
-    </div>
-  </div>
+    
+  
   <button type="submit" class="btn">Create</button>
 </form>
 <script type="text/javascript">
