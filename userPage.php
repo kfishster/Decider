cdf86e4c93749c79c844e031b3c6c5db462be28d
kfishster <kirillvar@gmail.com>
2013-04-01 00:05:52 -0500

    Changed home page

--- userPage.php
+++ userPage.php
@@ -23,24 +23,32 @@ $nameArray = explode(' ', $name);
 
 echo $nameArray[0].'!</h1><p>';
 
-$query = 'SELECT * FROM Event NATURAL JOIN User NATURAL JOIN Participates WHERE FBid = ' . $id;
+ $query = 'SELECT * FROM User WHERE FBid = ' . $id;
 
       
   $result = mysql_query($query) or die(mysql_error());
 
-  // Put the result in our own rows table.
-  for($i = 0; $row = @mysql_fetch_assoc($result); $i++) {
-    $rows[$i] = $row;
-  }
 
-  if(is_null($rows))
+  if(is_null($result))
   {
     //Put into database
     $query = 'INSERT INTO User VALUES('.$id.', "'.$email.'" , "'.$name.'");';
     $result = mysql_query($query) or die(mysql_error());
+  
   }
   else
   {
+    $query = 'SELECT * FROM Event NATURAL JOIN User NATURAL JOIN Participates WHERE FBid = ' . $id;
+
+      
+    $result = mysql_query($query) or die(mysql_error());
+
+
+    // Put the result in our own rows table.
+    for($i = 0; $row = @mysql_fetch_assoc($result); $i++) {
+      $rows[$i] = $row;
+    }
+
     foreach ($rows as $event) {
       echo '<li>'.$event['Title'].'</li>';
     }
