d5c7d2ed7716778eb78e93815d06c4eb2be45480
kfishster <kirillvar@gmail.com>
2013-04-01 04:02:55 -0500

    adding add event functionality

--- newEventForm.php
+++ newEventForm.php
@@ -2,9 +2,9 @@
 <h3>Create a new event</h3>
 <form class="form-horizontal" id="submitEvent">
   <div class="control-group">
-    <label class="control-label" for="inputEmail">Title</label>
+    <label class="control-label">Title</label>
     <div class="controls">
-      <input type="text" id="title" placeholder="Title">
+      <input type="text" id="titleinp" placeholder="Title">
     </div>
   </div>
   <button type="submit" class="btn">Create</button>
@@ -13,13 +13,13 @@
 
 $('#submitEvent').submit(function(event){
     event.preventDefault();
-    var form = $(this);
+    var $form = $(this);
 
-    title = form.find( 'input[name="title"]' ).val(),
+    title = $form.find( 'input[name="titleinp"]' ).val(),
     id = $('#getUserID').attr('userID');
     alert(title + " " + id);
 
-    $.post('./scripts/addEvent.php', {title: title, userID: id}, function (data){
+    $.post('./scripts/addEvent.php', {title: title, userID: id} , function (data){
 
     	alert(data);
       $('#eventList').append('<li><a class="openEvent" eventID="' + data + '">' + title + '</a></li>').slideDown();
