d8291459c5461d62f6d1eaadb82dc792d5a5bbf9
kfishster <kirillvar@gmail.com>
2013-03-31 19:05:23 -0500

    Updating index with bootstrap

--- index.php
+++ index.php
@@ -2,11 +2,18 @@
 <html lang="en">
 <head>
 <title>Oh hai</title>
-    
+    <link href="bootstrap/css/bootstrap.css" rel="stylesheet">
+    <link href="bootstrap/css/bootstrap-responsive.css" rel="stylesheet">
 </head>
 <body>
 
+<div class="container">
 <h1>Oh hai dur</h1>
+
+</div>
+
+<script src="bootstrap/js/jquery.js"></script>
+<script src="bootstrap/js/bootstrap.js"></script>
 
 </body>
 </html>
