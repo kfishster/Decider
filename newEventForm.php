4131f1ba8c42c99d8e803000211194c51b86900f
kfishster <kirillvar@gmail.com>
2013-04-01 04:15:08 -0500

    adding add event functionality

--- newEventForm.php
+++ newEventForm.php
@@ -19,8 +19,22 @@ $('#submitEvent').submit(function(event){
 
     $.post('./scripts/addEvent.php', {title: title, userID: id} , function (data){
 
-    	alert(data);
       $('#eventList').append('<li><a class="openEvent" eventID="' + data + '">' + title + '</a></li>').slideDown();
+
+      $('.openEvent').click(function(){
+
+        id = $(this).attr('eventID');
+        $('#eventContent').fadeOut("slow", function(){
+
+            $(this).load('eventPage.php',{id:id} ,function(){
+
+              $(this).fadeIn("slow");
+          });
+
+        });
+
+      });
+      
     });
 
   });
