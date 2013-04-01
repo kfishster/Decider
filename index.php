589527c7e80da12e374411406b3a4865bb674e92
kfishster <kirillvar@gmail.com>
2013-03-31 20:20:34 -0500

    testing FB login

--- index.php
+++ index.php
@@ -7,10 +7,50 @@
 </head>
 <body>
 
-<div class="container">
-<h1>Oh hai dur</h1>
 
-</div>
+<div id="fb-root"></div>
+
+<script>
+  // Additional JS functions here
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '153783808115722', // App ID
+      channelUrl : '//https://decider.azurewebsites.net//channel.html', // Channel File
+      status     : true, // check login status
+      cookie     : true, // enable cookies to allow the server to access the session
+      xfbml      : true  // parse XFBML
+    });
+
+    // Additional init code here
+
+  };
+
+  // Load the SDK Asynchronously
+  (function(d){
+     var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement('script'); js.id = id; js.async = true;
+     js.src = "//connect.facebook.net/en_US/all.js";
+     ref.parentNode.insertBefore(js, ref);
+   }(document));
+
+  function login() {
+    FB.login(function(response) {
+        if (response.authResponse) {
+            testAPI();
+        } else {
+            // cancelled
+        }
+    });
+	}
+
+	function testAPI() {
+    console.log('Welcome!  Fetching your information.... ');
+    FB.api('/me', function(response) {
+        console.log('Good to see you, ' + response.name + '.');
+    });
+}
+</script>
 
 <script src="bootstrap/js/jquery.js"></script>
 <script src="bootstrap/js/bootstrap.js"></script>
