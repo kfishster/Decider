39474c3a6f1f7cb80cd921ae6bc74ae86b5d5254
Robert P <robert.m.pieta@gmail.com>
2013-04-01 22:22:44 -0500

    API first draft and scaffold ios project added

---
+++ Decider-iOS/Decider-iOS/DCViewController.h
@@ -0,0 +1,13 @@
+//
+//  DCViewController.h
+//  Decider-iOS
+//
+//  Created by Robert Pieta on 4/1/13.
+//  Copyright (c) 2013 Robert Pieta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DCViewController : UIViewController
+
+@end
---
+++ Decider-iOS/Decider-iOS/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Decider-iOS
+//
+//  Created by Robert Pieta on 4/1/13.
+//  Copyright (c) 2013 Robert Pieta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DCAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DCAppDelegate class]));
+    }
+}
