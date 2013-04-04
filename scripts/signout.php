254e97632be11b07179ce92a177a64b39e042716
kfishster <kirillvar@gmail.com>
2013-04-03 23:00:45 -0500

    Adding a navbar with logout

--- scripts/signout.php
+++ scripts/signout.php
@@ -1,16 +1,19 @@
 <?php
 
+     echo "found";
   include('database_connection.php');
   include('cookies.php');
 
   if(validate_cookie()) 
   {
-      logout(getUserFBId());
-      header('Location: ~/index.php')
+         echo "in if";
+     // logout(getUserFBId());
+      //header('Location: ~/index.php')
   }
   else
   {
-    header('Location: ~/index.php');
+     echo "in else";
+    //header('Location: ~/index.php');
   }
 
 
