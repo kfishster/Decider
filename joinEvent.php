31a7f55c446474ea8a390f49850302de13769bd4
kfishster <kirillvar@gmail.com>
2013-04-04 00:42:09 -0500

    adding join events

--- joinEvent.php
+++ joinEvent.php
@@ -13,12 +13,13 @@
     $query = 'INSERT INTO Participates VALUES('.$id.', '.$event.')';
     $result = mysql_query($query) or die(mysql_error());
 
-    header( 'Location: /userPage.php?join=1' ) ;
+    echo 'Added!';
+    //header( 'Location: /userPage.php?join=1' ) ;
   }
   else
   {
-    
-    header( 'Location: /index.php?error=1' ) ;
+    echo 'else!';
+    //header( 'Location: /index.php?error=1' ) ;
   }
 
 
