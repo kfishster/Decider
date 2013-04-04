713bbf0be6d9e30a82d2c0d1105e2dd17f145211
kfishster <kirillvar@gmail.com>
2013-04-04 01:29:15 -0500

    adding modal

--- userPage.php
+++ userPage.php
@@ -201,9 +201,9 @@ $('#submitEvent').submit(function(event){
                   <form id="passForm" class="form-inline" method="post">
         
                   
-                  <input type="text" id="titleinp" placeholder="Title" class="input-xxlarge"><br>
-                  <input type="text" id="dateinp" placeholder="Date">
-                  <input type="text" id="timeinp" placeholder="Time"><br>
+                  <input type="text" id="titleinp" placeholder="Title" class="span6"><br>
+                  <input type="text" id="dateinp" placeholder="Date" class="span3">
+                  <input type="text" id="timeinp" placeholder="Time" class="span3"><br>
 
                   <button type="submit" class="btn">Submit</button>
             
