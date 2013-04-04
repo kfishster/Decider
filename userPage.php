7225cac83748ad0b7959d239532319a484a419d9
kfishster <kirillvar@gmail.com>
2013-04-04 03:05:30 -0500

    adding todo modal

--- userPage.php
+++ userPage.php
@@ -151,6 +151,13 @@ $('.openEvent').click(function(){
 
         $(this).fadeIn("slow");
 
+        $('.btn\ btn-info').click(function(){
+
+          $('#createTodoModal').modal('show');
+
+  
+        });
+
     });
 
   });
