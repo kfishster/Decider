6d181a6c233c98acae4286b9645937648aefb682
kfishster <kirillvar@gmail.com>
2013-04-10 00:40:09 -0500

    making the numbers change

--- userPage.php
+++ userPage.php
@@ -178,6 +178,7 @@ $('.openEvent').click(function(){
         $('.icon-thumbs-up').click(function(){
 
           newNum = parseInt($('#' + $(this).attr('path') + 'num').text());
+          alert(newNum);
           obj = $('#' + $(this).attr('path') + 'num');
 
           $.post('scripts/addPoint.php', {todo: $(this).attr('path'), userID: $('#getUserID').attr('userID'), point: '1'}, function(data){
