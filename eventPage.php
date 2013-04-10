14498cc98b0d90ff9c71a5c9b9b6e7857fb34978
kfishster <kirillvar@gmail.com>
2013-04-09 21:09:25 -0500

    adding voting

--- eventPage.php
+++ eventPage.php
@@ -43,8 +43,8 @@ foreach($rows as $todo)
 	$hash = hash('md4', $todo['Title'] + strval(rand()));
 	echo '<div class="accordion-group"> <div class="accordion-heading">';
 	echo '<div class="row-fluid"><div class="span10">';
-	echo '<h4><a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="#'.$hash.'">'.$todo['Title'].'  |   '.$todo['Points'].' points</a></h4>';
-	echo '</div><div class="span2"><b>0 points</b></div></div>';
+	echo '<h4><a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="#'.$hash.'">'.$todo['Title'].'</a></h4>';
+	echo '</div><div class="span2"><h4>'.$todo['Points'].' points</h4></div></div>';
 	echo '</div>';
 	
 	echo '<div id="'.$hash.'" class="accordion-body collapse"><div class="accordion-inner">';
