7cc42ce0e9ce3ab72290f99b9609373152399970
kfishster <kirillvar@gmail.com>
2013-04-10 00:03:15 -0500

    almost deree

--- scripts/addPoint.php
+++ scripts/addPoint.php
@@ -30,11 +30,13 @@ else, put it in the table.
     else
     {
       $query = 'UPDATE Points SET Point='.$point.' WHERE ToDoID='.$todo.';';
+      echo $query;
       
       $result = mysql_query($query) or die(mysql_error());
 
 
-      $query = 'UPDATE todo SET Points= Points + '.(2*$point).' WHERE ToDoID='.$todo.';';   
+      $query = 'UPDATE Todo SET Points= Points + '.(2*$point).' WHERE ToDoID='.$todo.';';   
+      echo $query;
 
       $result = mysql_query($query) or die(mysql_error());
 
