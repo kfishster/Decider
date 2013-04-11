176e686742bb25490b266cc5b777657dd5abf09b
kfishster <kirillvar@gmail.com>
2013-04-10 23:43:25 -0500

    adding api

--- API/event_todo.php
+++ API/event_todo.php
@@ -7,8 +7,8 @@
   $query = 'SELECT  EventID as eid, 
                     ToDoID as tdid,
                     Title as title,
-                    Points as pts,
-                    FROM Has NATURAL JOIN Todo WHERE EventID = ' . $eid .' ORDER BY Points DESC';
+                    Points as pts
+                    FROM Has NATURAL JOIN Todo WHERE EventID = ' . $eid .' ORDER BY Points DESC;';
 
   jsonify($query);
 
