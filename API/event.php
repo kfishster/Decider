bf99042cbdd2f88902c72af79772d41f531f3bc9
kfishster <kirillvar@gmail.com>
2013-04-11 02:28:57 -0500

    done for the day

--- API/event.php
+++ API/event.php
@@ -2,13 +2,27 @@
 
   include('dataManipulation.php');
 
-  $eid = $_GET['eid'];
- 
-	$query = 'SELECT 	EventID AS eid, 
-  						Admin AS admin_fbid, 
-  						Title AS title
-  						FROM Event WHERE EventID = '. $eid.';';  
+  if(isset($_GET['eid']))
+  {
+	  	$eid = $_GET['eid'];
+	 
+	  	$query = 'SELECT 	EventID AS eid, 
+	  						Admin AS admin_fbid, 
+	  						Title AS title
+	  						FROM Event WHERE EventID = '. $eid.';';  
 
-  jsonify($query, 'event');
+	  	jsonify($query, 'event');
+  }
+  else if(isset($_POST['adminfbid']))
+  {
+
+   	$adminfbid = $_POST['adminfbid'];
+   	$title = $_POST['title'];
+   	$description = $_POST['description']; 
+  
+  	$insert = 'INSERT INTO Event VALUES(NULL, '.$userID.', "'.$title.'", NULL, '.$description.')';
+
+  	insert($insert);
+  }
 
 ?>
