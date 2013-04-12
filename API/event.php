5e287e9c8e4163a919afc37ae867492c95153551
Kirill <kirillvar@gmail.com>
2013-04-12 16:29:24 -0500

    Not sure what is happening...

--- API/event.php
+++ API/event.php
@@ -2,28 +2,13 @@
 
   include('dataManipulation.php');
 
-  if(isset($_GET['eid']))
-  {
-	  	$eid = $_GET['eid'];
-	 
-	  	$query = 'SELECT 	EventID AS eid, 
-	  						Admin AS admin_fbid, 
-	  						Title AS title
-	  						FROM Event WHERE EventID = ?;';  
+  $eid = $_GET['eid'];
+   
+      $query = 'SELECT  EventID AS eid, 
+                Admin AS admin_fbid, 
+                Title AS title
+                FROM Event WHERE EventID = ?;';  
 
-	  	//jsonify($query, [$eid], 'event');
-
-  }
-  else if(isset($_POST['adminfbid']))
-  {
-
-   	$adminfbid = $_POST['adminfbid'];
-   	$title = $_POST['title'];
-   	$description = $_POST['description']; 
-  
-  	$insert = 'INSERT INTO Event VALUES(NULL, '.$userID.', "'.$title.'", NULL, '.$description.')';
-
-  	insert($insert);
-  }
+      jsonify($query, [$eid], 'event');
 
 ?>
