de4e4958bce9a66cd022ae2f559fb53a98b1bce3
Kirill <kirillvar@gmail.com>
2013-04-15 18:39:24 -0500

    Testing JSON

--- eventPage.php
+++ eventPage.php
@@ -20,6 +20,24 @@ $query = 'SELECT * FROM Has NATURAL JOIN Todo WHERE EventID = ' . $id .' ORDER B
 //Gets the admin of the event
 $eventQuery = 'SELECT * FROM Event INNER JOIN User ON Admin = FBid WHERE EventID = ' . $id;
 
+
+$url = 'API/event_todo.php';
+$data = array('eid' => $id);
+
+// use key 'http' even if you send the request to https://...
+$options = array(
+    'http' => array(
+        'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+        'method'  => 'POST',
+        'content' => http_build_query($data),
+    ),
+);
+$context  = stream_context_create($options);
+$result = file_get_contents($url, false, $context);
+
+var_dump($result);
+
+
       
 $result = mysql_query($query) or die(mysql_error());
 
