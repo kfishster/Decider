308fd03ccb4fc658ee46efe879654bbf00e8e144
Kirill <kirillvar@gmail.com>
2013-04-23 16:18:24 -0500

    Checking value

--- pageScripts.js
+++ pageScripts.js
@@ -324,7 +324,7 @@ and upon completetion, reload the event page.
             
             $('#location').blur();
             
-
+alert($('#location').val());
             if ($('#todoSubmitConf').attr('loc') == '0') {
 
                 $.getJSON('http://maps.googleapis.com/maps/api/geocode/json', {
@@ -332,8 +332,8 @@ and upon completetion, reload the event page.
                     sensor: 'false'
                 }, function (data) {
 
-                    $('#location').val(alert(data.results[0].formatted_address));
-                    $('#location').attr('stAddr', data.results[0].address_components[0].long_name + data.results[0].address_components[1].short_name);
+                    $('#location').val(data.results[0].formatted_address);
+                    $('#location').attr('stAddr', data.results[0].address_components[0].long_name + ' ' + data.results[0].address_components[1].short_name);
                     $('#location').attr('city', data.results[0].address_components[2].long_name);
                     $('#location').attr('state', data.results[0].address_components[5].short_name);
                     $('#location').attr('lat', data.results[0].geometry.location.lat);
