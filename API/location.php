5c2f8e2dc13c20243e24e81c3b9822e756d60368
Kirill <kirillvar@gmail.com>
2013-04-24 20:57:27 -0500

    Added location.php

---
+++ API/location.php
@@ -0,0 +1,21 @@
+<?php
+
+$lid = $_GET['lid'];
+	   
+	 	$query = 'SELECT  
+	 				Title AS title, 
+	                Street_Address AS address, 
+	                City AS city,
+	                State AS state,
+	                Longitutude AS lon,
+	                Latitude AS lat
+	                FROM Event WHERE LocationID = ?;';  
+
+
+		global $mysqli;
+	  
+	 	$stmt = $mysqli->prepare($query);
+	 	$stmt->bind_param('s', $lid);
+
+	    jsonify($stmt, 'location');
+?>
